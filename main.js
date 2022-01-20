3c087df1a6fb3647ef18daf5d80b518a74561485
Chris-dagostino <dagostino.christian94@gmail.com>
2022-01-20 16:02:22 +0100

    struttura

---
+++ main.js
@@ -0,0 +1,23 @@
+//chilometri da percorrere
+
+let km = prompt ('inserire i km da percorrere');
+
+//età del passeggero
+
+let eta = prompt ('inserisci la tua età');
+
+//prezzo totale del viaggio 0.21 al km
+
+let prezzo = (km * 0,21);
+document.getElementById('prezzo').innerHTML=prezzo
+
+//20% minorenni - 40% over65
+
+if { (eta < 18) 
+
+} else if { (eta > 65)
+
+} else {
+
+}
+
