1aebf3b93b07fed5c8e9e3a9b1f31965616aed4e
Chris-dagostino <dagostino.christian94@gmail.com>
2022-01-20 16:43:15 +0100

    prezzo

--- main.js
+++ main.js
@@ -6,16 +6,19 @@ let km = prompt ('inserire i km da percorrere');
 
 let eta = prompt ('inserisci la tua età');
 
-//prezzo totale del viaggio 0.21 al km
+//prezzo del viaggio 0.21 al km
 
 let prezzo = (km * 0,21);
 document.getElementById('prezzo').innerHTML=prezzo
 
 //20% minorenni - 40% over65
 
-if { (eta < 18) 
+if  (eta < 18) { 
+    let prezzo = prezzo - prezzo * 20/100
 
-} else if { (eta > 65)
+} else if (eta > 65) {
+    let prezzo = prezzo - prezzo * 40/100
+
 
 } else {
 
